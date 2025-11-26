9fa9718fecb34f4bba8368749b4674ae025e0eb9
lucialayr <lucia.sophie.layritz@posteo.de>
2025-11-25 17:54:49 -0800

    251125 pretty

--- code/251125.R
+++ code/251125.R
@@ -14,20 +14,33 @@ spi_indicators = tuesdata$spi_indicators %>%
   sf::st_as_sf()
 
 population_data = spi_indicators %>%
+  filter(year == 2023) %>%
   dplyr::select(country, region, population) %>%
+  unique() %>%
   sf::st_transform(crs = 4326) %>%
   mutate(area = sf::st_area(geometry),
          x = sf::st_coordinates(sf::st_centroid(geometry))[,1],
-         y = sf::st_coordinates(sf::st_centroid(geometry))[,2])
+         y = sf::st_coordinates(sf::st_centroid(geometry))[,2],
+         population = population/1000000) %>%
+  arrange(desc(population)) 
+
+colors = scico(n = 6, palette = "batlow", direction = 1, end = .8)
+legend_color = colors[6]
 
 ggplot() + theme_void() +
   geom_sf(data = spi_indicators, color = 'white',  fill = 'grey90', linewidth = .1) +
   geom_point(data = population_data, aes(x = x, y = y, size = population, fill = region),
              color = 'grey90', shape = 21, stroke = .5) +
-  scale_size_continuous(range = c(0.5, 40)) +
+  scale_size_continuous(range = c(0.5, 40),
+                        breaks = c(1, 100, 1000),
+                        limits = c(0, 1500)) +
   scico::scale_fill_scico_d(palette = 'batlow', end = .8) +
   scale_x_continuous(expand = c(0,0)) +
   scale_y_continuous(expand = c(0,0)) +
   theme(plot.background = element_rect(fill = 'grey90', colour = NA)) +
-  theme(legend.position = 'None')
+  guides(fill = guide_none(),
+         size = guide_legend(title = 'Population in Millions (2023)', title.position = 'top', title.hjust = .5,
+                             override.aes = list(fill = legend_color)))
+
+ggsave('viz/251125.png', dpi = 300, height = 6, width = 12)
 
